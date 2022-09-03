baeb3e32072b8343ad20ee07404b6278e8072331
Quentin Glorieux <quentin.glorieux@gmail.com>
2022-09-03 23:29:32 +0200

    first commit

--- index.php
+++ index.php
@@ -4,6 +4,6 @@ Plugin Name: Rb Web Portal API
 Plugin URI: www.rubidiumweb.fr
 Description: RbW plugin for portal synchronization via the API
 Author: Rb87
-Version: 0.0.2
+Version: 0.0.1
 Author URI: http://www.rubidiumweb.fr/
 */
